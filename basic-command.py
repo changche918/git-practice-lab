e705975f8a84a65c379fe8f0b35ce9592d205dc2
Ryan <changche918@gmail.com>
2026-04-05 17:14:01 +0800

    222

--- basic-command.py
+++ basic-command.py
@@ -1,14 +1,21 @@
 # 20260405
 '''
-- git restore file.txt (還原最後一次 file.txt 檔案的 commit)
-    - 如果你修改了一個檔案，但還沒執行 git add，現在後悔了想變回修改前的樣子：
-    - 語法： git restore <檔案名稱>
-    - 效果： 丟棄目前的修改，檔案內容會變回上一次 Commit 或暫存時的狀態。
-
-    - 如果你已經執行了 git add 把檔案送進暫存區（Staging Area），但突然發現還有東西沒改好，想把它從暫存區拿出來：
-    - 語法： git restore --staged <檔案名稱>
-    - 效果： 檔案會從暫存區退回到工作區，但修改的內容會保留。
-    - 常用場景： 不小心 git add . 加到不想提交的檔案，想把它踢出這次的 Commit 清單。
+- git restore
+    """ 
+    方法1
+    如果你修改了一個檔案，但還沒執行 git add，現在後悔了想變回修改前的樣子：
+    語法： git restore <檔案名稱>
+    效果： 丟棄目前的修改，檔案內容會變回上一次 Commit 或暫存時的狀態。
+    ---
+    方法2:
+    如果你已經執行了 git add 把檔案送進暫存區（Staging Area），但突然發現還有東西沒改好，想把它從暫存區拿出來：
+    語法： git restore --staged <檔案名稱>
+    效果： 檔案會從暫存區退回到工作區，但修改的內容會保留。
+    常用場景： 不小心 git add . 加到不想提交的檔案，想把它踢出這次的 Commit 清單。
+    ---
+    方法3:
+    如果你想讓某個檔案回到更早之前的某個版本（而不是最近的一次)
+    語法： git restore --source <Commit ID> <檔案名稱>
 - git merge : 把分支合併 git checkout main、git merge feature
 - git rebase : 把你的 commit「接到別的分支後面」，git checkout feature、git rebase main
 - git reset : 回到某個版本 
