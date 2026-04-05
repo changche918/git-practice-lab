dd92af7ae3c131101a8841d273c9def082ca6b8c
Ryan <changche918@gmail.com>
2026-04-05 16:29:57 +0800

    first-commit

---
+++ basic-command.py
@@ -0,0 +1,10 @@
+# 20260405
+'''
+- git restore file.txt (還原最後一次 file.txt 檔案的 commit)
+- git merge : 把分支合併 git checkout main、git merge feature
+- git rebase : 把你的 commit「接到別的分支後面」，git checkout feature、git rebase main
+- git reset : 回到某個版本 
+- git revert <commit_id> : 清掉上一個 commit 點
+'''
+
+print("1. PRINT 輸出")
